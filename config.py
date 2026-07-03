2de4ee749ef7ee37cad294082ee85b44e568c8bf
anushaanupittala-lgtm <anushaanupittala@gmail.com>
2026-07-03 17:49:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+STOCKS = [
+    "RELIANCE.NS",
+    "TCS.NS",
+    "INFY.NS",
+    "HDFCBANK.NS",
+    "ICICIBANK.NS",
+    "SBIN.NS",
+    "LT.NS",
+    "ITC.NS",
+    "AXISBANK.NS",
+    "HINDUNILVR.NS"
+]
